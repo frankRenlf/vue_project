c59a8440a6c9201dea55649abcf07fd4092e3219
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-30 18:59:10 +0800

    Update main.js

--- vue_test_demo/src/main.js
+++ vue_test_demo/src/main.js
@@ -1,11 +1,16 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   el:'#app',
+  // template:`<h1>hello</h1>`,
   // render: h => h(App),
+
+  render(h) {
+    return h(App)
+  },
   components:{
     App,
   }
