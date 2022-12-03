aa2c2e16d29fa4acb4f8db83b43066b7da7e4617
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-03 18:33:58 +0800

    update

---
+++ vue_test_demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ElementOne />
+  </div>
+</template>
+
+<script>
+import ElementOne from "./components/ElementOne.vue";
+export default {
+  name: "App",
+  components: {
+    ElementOne,
+  },
+};
+</script>
+
+<style>
+</style>
--- vue_test_demo/src/main.js
+++ vue_test_demo/src/main.js
@@ -1,1 +1,12 @@
 import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    el:'#root',
+    data() {
+        return {
+            
+        }
+    },
+    render:h=>h(App)
+})
