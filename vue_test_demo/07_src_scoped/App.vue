0d1284006dc47e627331587c897ecb82995a73be
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-04 19:40:46 +0800

    update

---
+++ vue_test_demo/07_src_scoped/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2 v-text="msg" ref="info"></h2>
+    <button @click="showDOM">click</button>
+    <hr />
+    <SchoolInfo />
+    <hr />
+    <StudentInfo />
+
+    <!-- <ElementOne /> -->
+  </div>
+</template>
+
+<script>
+import StudentInfo from "./components/StudentInfo.vue";
+import SchoolInfo from "./components/SchoolInfo.vue";
+export default {
+  name: "App",
+  components: {
+    StudentInfo,
+    SchoolInfo,
+  },
+  data() {
+    return {
+      msg: "welcome to learn vue",
+    };
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.info);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
