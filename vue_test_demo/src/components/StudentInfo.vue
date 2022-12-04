5409c6336c15ee283f022fa04b2d0187e0329bea
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-04 14:21:00 +0800

    Update StudentInfo.vue

--- vue_test_demo/src/components/StudentInfo.vue
+++ vue_test_demo/src/components/StudentInfo.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-    <h2>{{ cur_name }}</h2>
-    <h2>{{ cur_gender }}</h2>
-    <h2>{{ cur_address }}</h2>
-    <h2>{{ cur_age }}</h2>
-    <button @click="cur_age++">age++</button>
-    <ul>
-      <li v-for="(user, a) in users" :key="user.id">
-        {{ user.id }}-{{ user.name }}-{{ a }}
-      </li>
-    </ul>
+    <h2>{{ name }}</h2>
+    <h2>{{ age }}</h2>
+    <button @click="add">age++</button>
   </div>
 </template>
 
@@ -17,43 +10,14 @@
 export default {
   name: "StudentInfo",
   data() {
-    // console.log(this);
     return {
-      identify: "student",
-      cur_name: this.name,
-      cur_gender: this.gender,
-      cur_address: this.address,
-      cur_age: this.age,
-      users: [
-        { id: 1, name: "a" },
-        { id: 2, name: "b" },
-        { id: 3, name: "c" },
-      ],
+      name: "frank",
+      age: 2,
     };
   },
-  // props: ["name", "gender", "address", "age"],
-  // props: {
-  //   name: String,
-  //   gender: String,
-  //   address: String,
-  //   age: Number,
-  // },
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    age: {
-      type: Number,
-      required: true,
+  methods: {
+    add() {
+      this.age++;
     },
   },
 };
