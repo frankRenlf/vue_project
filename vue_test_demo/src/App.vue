ac735edde5b7d8d31cabd448b75801ff84f9aa69
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-07 16:05:10 +0800

    update

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -1,120 +1,37 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <HeaderElement :addTodo="addTodo"></HeaderElement>
-        <ListElement :todoList="todoList"
-                     :checkTodo="checkTodo"
-                     :deleteTodo="deleteTodo">
-        </ListElement>
-        <footer-element :todoList="todoList"
-                        :checkAllTodo="checkAllTodo"
-                        :clearAllTodo="clearAllTodo">
-        </footer-element>
-      </div>
-    </div>
+  <div>
+    <h2 v-text="msg" ref="info"></h2>
+    <button @click="showDOM">click</button>
+    <hr />
+    <SchoolInfo />
+    <hr />
+    <StudentInfo />
+
+    <!-- <ElementOne /> -->
   </div>
 </template>
 
 <script>
-import HeaderElement from "@/components/HeaderElement.vue";
-import ListElement from "@/components/ListElement.vue";
-import FooterElement from "@/components/FooterElement.vue";
-
+import StudentInfo from "./components/StudentInfo.vue";
+import SchoolInfo from "./components/SchoolInfo.vue";
 export default {
   name: "App",
   components: {
-    HeaderElement,
-    ListElement,
-    FooterElement,
+    StudentInfo,
+    SchoolInfo,
   },
   data() {
     return {
-      msg: "welcome to todolist vue",
-      todoList: JSON.parse(localStorage.getItem('todoList')) || []
+      msg: "welcome to learn vue",
     };
   },
   methods: {
-    addTodo(x) {
-      this.todoList.unshift(x)
+    showDOM() {
+      console.log(this.$refs.info);
     },
-    checkTodo(id) {
-      this.todoList.forEach((todo) => {
-        if (todo.id === id) todo.completed = !todo.completed
-      })
-    },
-    deleteTodo(id) {
-      this.todoList = this.todoList.filter((todo) => {
-        return todo.id !== id
-      })
-    },
-    checkAllTodo(checked) {
-      this.todoList.forEach((todo) => {
-        todo.completed = checked
-      })
-    },
-    clearAllTodo() {
-      this.todoList = this.todoList.filter((todo) => {
-        return !todo.completed
-      })
-    }
   },
-  watch: {
-    todoList: {
-      deep: true,
-      handler(value) {
-        localStorage.setItem('todoList', JSON.stringify(value))
-      }
-    }
-
-  }
-
 };
 </script>
 
 <style>
-/*base*/
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
 </style>
---
+++ vue_test_demo/src/components/SchoolInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h2>{{ name }}</h2>
+    <h2>{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SchoolInfo",
+  data() {
+    return {
+      name: "leeds",
+      address: "british",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.demo {
+  background-color: skyblue;
+}
+</style>
