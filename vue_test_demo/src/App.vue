044c802419a4f8ae2a60190202b9548b4dedc8fc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-09 11:08:26 +0800

    Update App.vue

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h2 v-text="msg" ref="info"></h2>
+    <h2>stu: {{ stu }} </h2>
     <button @click="showDOM">click</button>
     <hr/>
     <SchoolInfo :passByProps="passByProps"/>
     <hr/>
     <StudentInfo @denote="receiveData"/>
     <hr/>
-    <StudentInfo ref="student"/>
+    <StudentInfo ref="student" @click.native="show"/>
     <!-- <ElementOne /> -->
   </div>
 </template>
@@ -25,6 +26,7 @@ export default {
   data() {
     return {
       msg: "welcome to learn vue",
+      stu: ''
     };
   },
   methods: {
@@ -36,10 +38,17 @@ export default {
     },
     passByProps(val) {
       console.log(val)
+    },
+    show(){
+      alert(111)
     }
   },
   mounted() {
-    this.$refs.student.$on('denote', this.passByProps)
+    this.$refs.student.$on('denote', (val) => {
+      console.log(val)
+      console.log(this)
+      this.stu = val
+    })
   }
 };
 </script>
