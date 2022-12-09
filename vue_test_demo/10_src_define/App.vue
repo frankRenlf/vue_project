3623524650ceb8d77cfd9b6135b8e6b97af8fe6c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-09 19:27:47 +0800

    update

---
+++ vue_test_demo/10_src_define/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2 v-text="msg" ref="info"></h2>
+    <h2>stu: {{ stu }} </h2>
+    <button @click="showDOM">click</button>
+    <hr/>
+    <SchoolInfo :passByProps="passByProps"/>
+    <hr/>
+    <StudentInfo @denote="receiveData"/>
+    <hr/>
+    <StudentInfo ref="student" @click.native="show"/>
+    <!-- <ElementOne /> -->
+  </div>
+</template>
+
+<script>
+import StudentInfo from "./components/StudentInfo.vue";
+import SchoolInfo from "./components/SchoolInfo.vue";
+
+export default {
+  name: "App",
+  components: {
+    StudentInfo,
+    SchoolInfo,
+  },
+  data() {
+    return {
+      msg: "welcome to learn vue",
+      stu: ''
+    };
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.info);
+    },
+    receiveData(val) {
+      console.log(val)
+    },
+    passByProps(val) {
+      console.log(val)
+    },
+    show(){
+      alert(111)
+    }
+  },
+  mounted() {
+    this.$refs.student.$on('denote', (val) => {
+      console.log(this)
+      this.stu = val
+    })
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue_test_demo/src/components/FooterElement.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="todo-footer" v-show="total!==0">
+    <label>
+      <!--      <input type="checkbox" :checked="isAll" @change="checkAll">-->
+      <input type="checkbox" v-model="isAll">
+    </label>
+    <span>
+          <span>have completed {{ completedTask }} / total {{ total }}</span>
+      <!--          <span> Completion rate{{ (completedTask / todoList.length) }}</span>-->
+    </span>
+    <button class="btn btn-danger" @click="clearAll">clear completed task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterElement",
+  props: ['todoList', 'checkAllTodo', 'clearAllTodo'],
+  methods: {
+    checkAll(e) {
+      this.checkAllTodo(e.target.checked)
+    },
+    clearAll() {
+      this.clearAllTodo()
+    }
+  },
+  computed: {
+    completedTask() {
+      return this.todoList.reduce((pre, cur) => {
+        return pre + cur.completed
+      }, 0)
+    },
+    total() {
+      return this.todoList.length
+    },
+    isAll: {
+      get() {
+        return this.completedTask === this.total && this.total !== 0
+      },
+      set(val) {
+        this.checkAllTodo(val)
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
