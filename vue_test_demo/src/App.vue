05b3d97e9bcdf4352c8a57d32553900e1fb86298
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-10 15:29:09 +0800

    update

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -2,88 +2,18 @@
   <div id="root">
     <div class="todo-container">
       <div class="todo-wrap">
-        <HeaderElement @addTodo="addTodo"></HeaderElement>
-        <ListElement :todoList="todoList">
-        </ListElement>
-        <footer-element :todoList="todoList"
-                        @checkAllTodo="checkAllTodo"
-                        @clearAllTodo="clearAllTodo">
-        </footer-element>
+        <TestElement></TestElement>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HeaderElement from "@/components/HeaderElement.vue";
-import ListElement from "@/components/ListElement.vue";
-import FooterElement from "@/components/FooterElement.vue";
+import TestElement from "@/components/TestElement.vue";
 
 export default {
   name: "App",
-  components: {
-    HeaderElement,
-    ListElement,
-    FooterElement,
-  },
-  data() {
-    return {
-      msg: "welcome to todolist vue",
-      todoList: JSON.parse(localStorage.getItem('todoList')) || []
-    };
-  },
-  methods: {
-    addTodo(x) {
-      this.todoList.unshift(x)
-    },
-    checkTodo(id) {
-      this.todoList.forEach((todo) => {
-        if (todo.id === id) todo.completed = !todo.completed
-      })
-    },
-    deleteTodo(id) {
-      this.todoList = this.todoList.filter((todo) => {
-        return todo.id !== id
-      })
-    },
-    checkAllTodo(checked) {
-      this.todoList.forEach((todo) => {
-        todo.completed = checked
-      })
-    },
-    clearAllTodo() {
-      this.todoList = this.todoList.filter((todo) => {
-        return !todo.completed
-      })
-    },
-    updateTodo(id, isEdit, content) {
-      this.todoList.forEach((todo) => {
-        if (todo.id === id) {
-          todo.isEdit = isEdit === null ? todo.isEdit : isEdit
-          todo.content = content === null ? todo.content : content
-        }
-      })
-    }
-  },
-  watch: {
-    todoList: {
-      deep: true,
-      handler(value) {
-        localStorage.setItem('todoList', JSON.stringify(value))
-      }
-    }
-
-  },
-  mounted() {
-    this.$root.$on('checkTodo', this.checkTodo)
-    this.$root.$on('deleteTodo', this.deleteTodo)
-    this.$root.$on('updateTodo', this.updateTodo)
-  },
-  beforeDestroy() {
-    this.$root.$off('checkTodo')
-    this.$root.$off('deleteTodo')
-    this.$root.$off('updateTodo')
-  }
+  components: {TestElement}
 
 };
 </script>
---
+++ vue_test_demo/src/components/TestElement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="isShow=!isShow">Show/Hide</button>
+    <h1 v-show="isShow">hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestElement",
+  data() {
+    return {
+      isShow: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
