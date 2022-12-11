314834341756557092ee59cd3e71047fda77f9df
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-11 18:34:17 +0800

    update

---
+++ vue_test_demo/20_src_github_search/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <SearchComponent/>
+    <ListComponent/>
+  </div>
+</template>
+
+<script>
+
+import SearchComponent from "@/components/SearchComponent.vue";
+import ListComponent from "@/components/ListComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    SearchComponent,
+    ListComponent,
+  },
+  methods: {
+
+  }
+
+};
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-edit {
+  color: #fff;
+  background-color: #5ac1b8;
+  border: 1px solid #3b856e;
+  margin-right: 5px;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+</style>
