317d025497cd3aa9e0020941e0546e8aed08e186
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-11 15:43:58 +0800

    update

---
+++ vue_test_demo/19_src_axios/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="root">
+    <button @click="listStudent">obtain student info</button>
+    <hr/>
+    <button @click="listCar">obtain car info</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: "App",
+  methods: {
+    listStudent() {
+      axios.get('/api/student/list').then(
+          response => {
+            console.log('request success', response.data)
+          },
+          error => {
+            console.log('request fail', error.message)
+          }
+      )
+    },
+    listCar() {
+      axios.get('/demo/car/list').then(
+          response => {
+            console.log('request success', response.data)
+          },
+          error => {
+            console.log('request fail', error.message)
+          }
+      )
+    }
+  }
+
+};
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-edit {
+  color: #fff;
+  background-color: #5ac1b8;
+  border: 1px solid #3b856e;
+  margin-right: 5px;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+</style>
---
+++ vue_test_demo/19_src_axios/components/ListElement.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="todo-main">
+    <transition-group :appear="true"
+                      name="animate__animated animate__bounce"
+                      enter-active-class="animate__fadeInDown"
+                      leave-active-class="animate__fadeOutUp">
+      <ItemElement v-for="todo in todoList"
+                   :key="todo.id"
+                   :todo="todo">
+        {{ todo }}
+      </ItemElement>
+    </transition-group>
+  </ul>
+</template>
+
+<script>
+import 'animate.css'
+import ItemElement from "@/components/ItemElement.vue";
+
+export default {
+  name: "ListElement",
+  components: {
+    ItemElement,
+  },
+  props: {
+    todoList: {
+      type: Array
+    },
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 2px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
