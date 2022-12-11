3b6ed3b2cf30888d144c6c9cfd80a560e6a4048a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-11 16:35:07 +0800

    Update SearchComponent.vue

--- vue_test_demo/src/components/SearchComponent.vue
+++ vue_test_demo/src/components/SearchComponent.vue
@@ -2,7 +2,9 @@
   <section class="jumbotron">
     <h3 class="jumbotron-heading">Search Github Users</h3>
     <div>
-      <input type="text" placeholder="enter the name you search" v-model="keyWord"/>&nbsp;
+      <input type="text" placeholder="enter the name you search"
+             v-model="keyWord"
+             @keydown.enter="searchUsers"/>&nbsp;
       <button @click="searchUsers">Search</button>
     </div>
   </section>
@@ -18,9 +20,16 @@ export default {
       keyWord: '',
     }
   },
-  methods:{
-    searchUsers(){
-      axios.get('')
+  methods: {
+    searchUsers() {
+      axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+          response => {
+            console.log(response.data)
+          },
+          error => {
+            console.log(error.message)
+          }
+      )
     }
   }
 }
