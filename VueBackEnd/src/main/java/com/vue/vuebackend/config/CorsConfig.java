8c613bb2e9a922c177407490ba6469b1d0736075
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-11 12:19:08 +0800

    Update CorsConfig.java

--- VueBackEnd/src/main/java/com/vue/vuebackend/config/CorsConfig.java
+++ VueBackEnd/src/main/java/com/vue/vuebackend/config/CorsConfig.java
@@ -37,7 +37,7 @@ public class CorsConfig {
                         .allowCredentials(true)                             // 是否发送Cookie信息
                         .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 放行哪些请求方式
                         .allowedHeaders("*")                                // 放行哪些原始域(头部信息)
-                        .exposedHeaders("Header1", "Header2")               // 暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                        .exposedHeaders("*")               // 暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
                         .maxAge(3600);                                      // 预请求的结果有效期，默认1800分钟,3600是一小时
             }
         };
