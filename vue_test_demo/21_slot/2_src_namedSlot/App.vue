433b35d515adf9b3cae767f211627b35d998a213
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-12 14:25:11 +0800

    update

---
+++ vue_test_demo/21_slot/2_src_namedSlot/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="category">
+    <div class="container">
+      <CategoryComponent title="美食">
+        <template v-slot:main>
+          <img
+              src="https://avatars.githubusercontent.com/u/89248496?s=400&u=fca6dcbb2917ad143245a2c2fb53df98fa72fabc&v=4"
+              alt="delicious food"/>
+        </template>
+        <template v-slot:footer>
+          <a href="https://baidu.com">baidu</a>
+        </template>
+
+      </CategoryComponent>
+
+      <CategoryComponent title="游戏" :listData="games">
+        <template v-slot:main>
+          <ul>
+            <li v-for="(g , index) in games" :key="index">{{ g }}</li>
+          </ul>
+        </template>
+        <template v-slot:footer>
+          <a href="https://baidu.com">baidu</a>
+          <a href="https://baidu.com">baidu</a>
+        </template>
+      </CategoryComponent>
+
+      <CategoryComponent title="电影">
+        <template v-slot:main>
+          <video src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" controls></video>
+        </template>
+        <template v-slot:footer>
+          <a href="https://baidu.com">baidu</a>
+          <a href="https://baidu.com">baidu</a>
+          <a href="https://baidu.com">baidu</a>
+        </template>
+
+      </CategoryComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import CategoryComponent from "@/components/CategoryComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    CategoryComponent
+  },
+  data() {
+    return {
+      foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+      games: ['红色警戒', '穿越火线', '劲舞团', '超级玛丽'],
+      films: ['《教父》', '《拆弹专家》', '《你好，李焕英》', '《尚硅谷》']
+    }
+  },
+  methods: {}
+
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+
+video {
+  width: 100%;
+}
+
+img {
+  width: 100%;
+}
+</style>
---
+++ vue_test_demo/21_slot/2_src_namedSlot/components/CategoryComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <!--插槽,等着组件的使用者进行填充-->
+    <slot name="main">main area</slot>
+    <slot name="footer">footer area</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryComponent",
+  props: ['listData', 'title']
+}
+</script>
+
+<style scoped>
+.category {
+  background: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+h3 {
+  text-align: center;
+  background: orange;
+}
+
+img {
+  width: 100%;
+}
+</style>
