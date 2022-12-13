827255dc0f5ac4e038ea654ec58e6917deb395fe
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-13 16:12:08 +0800

    update

--- vue_test_demo/src/main.js
+++ vue_test_demo/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from "axios";
+import vuex from 'vuex'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
+Vue.use(vuex)
 new Vue({
     el: '#app',
     data() {
