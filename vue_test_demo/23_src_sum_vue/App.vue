04c0ba2c97fd4f63846034097d601eb3628fdadf
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-13 14:46:08 +0800

    update

---
+++ vue_test_demo/23_src_sum_vue/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <CountComponent></CountComponent>
+  </div>
+</template>
+
+<script>
+
+import CountComponent from "@/components/CountComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    CountComponent
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+
+};
+</script>
+
+<style scoped>
+
+</style>
