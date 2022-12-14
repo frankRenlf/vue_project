70c47fe362299896bd5e7aedf6566e9d29dfcbbc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-14 15:34:02 +0800

    Update index.js

--- vue_test_demo/src/store/index.js
+++ vue_test_demo/src/store/index.js
@@ -3,44 +3,59 @@ import Vue from 'vue'
 
 Vue.use(vuex)
 
-const actions = {
-    add(context, value) {
-        context.commit('ADD', value)
+const countOptions = {
+    actions: {
+        add(context, value) {
+            context.commit('ADD', value)
+        },
+        delete(context, value) {
+            context.commit('DELETE', value)
+        },
+        addOdd(context, value) {
+            console.log('odd', value)
+            if (state.sum % 2 !== 0) {
+                context.commit('ADD', value.n)
+            }
+        },
+        addWait(context, value) {
+            console.log('wait', value)
+            setTimeout(() => {
+                context.commit('ADD', value)
+            }, 500)
+        },
     },
-    delete(context, value) {
-        context.commit('DELETE', value)
+    mutations: {
+        ADD(state, value) {
+            // console.log('value', value)
+            state.sum += value
+        },
+        DELETE(state, value) {
+            state.sum -= value
+        },
     },
-    addOdd(context, value) {
-        console.log('odd', value)
-        if (state.sum % 2 !== 0) {
-            context.commit('ADD', value.n)
+    state: {
+        sum: 0,
+        school: 'swjtu',
+        subject: 'math',
+    },
+    getters: {}
+}
+const personOptions = {
+    actions: {},
+    mutations: {
+        ADD_PERSON(state, value) {
+            state.personList.push(value)
         }
     },
-    addWait(context, value) {
-        console.log('wait', value)
-        setTimeout(() => {
-            context.commit('ADD', value)
-        }, 500)
+    state: {
+        personList: [],
     },
+    getters: {}
 }
-const mutations = {
-    ADD(state, value) {
-        // console.log('value', value)
-        state.sum += value
-    },
-    DELETE(state, value) {
-        state.sum -= value
-    },
-    ADD_PERSON(state,value){
-        state.personList.push(value)
-    }
-}
-const state = {
-    sum: 0,
-    school: 'swjtu',
-    subject: 'math',
-    personList: [],
-}
+
+const actions = {}
+const mutations = {}
+const state = {}
 const getters = {
     countSum(state) {
         return state.sum * 10
