4f1cc68fed031034b1f8ff85123248c633b9c651
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-15 16:15:09 +0800

    Update App.vue

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -10,9 +10,10 @@
           <!--          <a class="list-group-item active" href="./about.html">About</a>-->
           <!--          <a class="list-group-item" href="./home.html">Home</a>-->
           <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
-          <router-link class="list-group-item" active-class="active" :to="{
-            name:'aboutinfo'
-          }">
+          <router-link class="list-group-item"
+                       active-class="active"
+                       :to="{name:'aboutinfo'}"
+          >
             About
           </router-link>
 
