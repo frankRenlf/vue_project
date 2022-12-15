990b2ab9855ef55c7da710080297fa54879c5d9a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-15 13:55:53 +0800

    update

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -1,21 +1,42 @@
 <template>
   <div>
-    <CountComponent></CountComponent>
-    <hr/>
-    <PersonComponent></PersonComponent>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>Vue Router Demo</h2></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!--          原始使用a标签跳转多个页面,多页面应用-->
+          <a class="list-group-item active" href="./about.html">About</a>
+          <a class="list-group-item" href="./home.html">Home</a>
+
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!--router-view确定视图的位置-->
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
 
-import CountComponent from "@/components/CountComponent.vue";
-import PersonComponent from "@/components/PersonComponent.vue";
+import HomeComponent from "@/components/HomeComponent.vue";
+import AboutComponent from "@/components/AboutComponent.vue";
 
 export default {
   name: "App",
   components: {
-    CountComponent,
-    PersonComponent
+    HomeComponent,
+    AboutComponent
   },
   data() {
     return {}
