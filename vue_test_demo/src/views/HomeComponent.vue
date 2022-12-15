265eb26a112bfacb8efca3206ed3173598da8382
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-15 15:08:50 +0800

    Update HomeComponent.vue

--- vue_test_demo/src/views/HomeComponent.vue
+++ vue_test_demo/src/views/HomeComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h2>home</h2>
-      <router-link to="/msg">msg</router-link>
+      <router-link to="/msg">msg</router-link>&nbsp;
       <router-link to="/news">news</router-link>
       <router-view></router-view>
     </div>
