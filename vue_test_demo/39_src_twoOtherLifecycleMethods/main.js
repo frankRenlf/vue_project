079d9bef4d4cf0da6af48e39970fac393bc896ad
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-17 15:42:29 +0800

    add

---
+++ vue_test_demo/39_src_twoOtherLifecycleMethods/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from "@/App.vue";
+import axios from "axios";
+
+import store from "@/store";
+import router from '@/router';
+
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+
+new Vue({
+    el: '#app',
+    data() {
+        return {}
+    },
+    render: h => h(App),
+    store: store,
+    router: router,
+    mounted() {
+        console.log('$router', this.$router)
+        console.log('$route', this.$route)
+    }
+})
