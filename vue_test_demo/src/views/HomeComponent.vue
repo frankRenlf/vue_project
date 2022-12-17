c3c437a369edaf7f182c9671eb532c11633972bd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-17 14:53:58 +0800

    Update HomeComponent.vue

--- vue_test_demo/src/views/HomeComponent.vue
+++ vue_test_demo/src/views/HomeComponent.vue
@@ -3,7 +3,7 @@
     <h2>home</h2>
     <router-link to="/home/msg">msg</router-link>&nbsp;
     <router-link to="/home/news">news</router-link>
-    <keep-alive>
+    <keep-alive include="news">
       <router-view></router-view>
     </keep-alive>
 
