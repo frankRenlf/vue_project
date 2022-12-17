4d982f943b1808d206a896845a08a7540fecedb8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-17 10:32:46 +0800

    Update HomeMessageDetail.vue

--- vue_test_demo/src/views/HomeMessageDetail.vue
+++ vue_test_demo/src/views/HomeMessageDetail.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <h4>msg :{{$route.params.id}}---{{$route.params.name}}</h4>
+    <h4>msg :{{ $route.params.id }}---{{ $route.params.name }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeMessageDetail",
-  data(){
-    return{
-    }
+  data() {
+    return {}
   },
+  computed: {
+    params() {
+      return this.$route.params
+    }
+
+  }
 
 
 }
