de8f83cd7e8e8359b0bb6d3beb6e54b6e5000eab
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-17 14:33:19 +0800

    Update BannerComponent.vue

--- vue_test_demo/src/components/BannerComponent.vue
+++ vue_test_demo/src/components/BannerComponent.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="col-xs-offset-2 col-xs-8">
-    <div class="page-header"><h2>Vue Router Demo</h2></div>
+    <div class="page-header">
+      <h2>Vue Router Demo</h2>
+      <button @click="back">back</button>
+      <button @click="forward">forward</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "BannerComponent"
+  name: "BannerComponent",
+  methods: {
+    back() {
+      this.$router.back()
+    },
+    forward() {
+      this.$router.forward()
+    }
+  }
 }
 </script>
 
