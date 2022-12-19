6135a723f151bd8e2293cbaee5c71ae7b3d6714d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-19 14:38:37 +0800

    Update App.vue

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -1,40 +1,24 @@
 <template>
   <div>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button plain>朴素按钮</el-button>
-      <el-button type="primary" plain>主要按钮</el-button>
-      <el-button type="success" plain>成功按钮</el-button>
-      <el-button type="info" plain>信息按钮</el-button>
-      <el-button type="warning" plain>警告按钮</el-button>
-      <el-button type="danger" plain>危险按钮</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button round>圆角按钮</el-button>
-      <el-button type="primary" round>主要按钮</el-button>
-      <el-button type="success" round>成功按钮</el-button>
-      <el-button type="info" round>信息按钮</el-button>
-      <el-button type="warning" round>警告按钮</el-button>
-      <el-button type="danger" round>危险按钮</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button icon="el-icon-search" circle></el-button>
-      <el-button type="primary" icon="el-icon-edit" circle></el-button>
-      <el-button type="success" icon="el-icon-check" circle></el-button>
-      <el-button type="info" icon="el-icon-message" circle></el-button>
-      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle></el-button>
-    </el-row>
+    <div class="block">
+      <!--      <span class="demonstration">默认</span>-->
+      <el-date-picker
+          v-model="value1"
+          type="date"
+          placeholder="选择日期">
+      </el-date-picker>
+      <button @click="showInfo">showInfo</button>
+    </div>
+    <div class="block">
+      <!--      <span class="demonstration">带快捷选项</span>-->
+      <el-date-picker
+          v-model="value2"
+          align="right"
+          type="date"
+          placeholder="选择日期"
+          :picker-options="pickerOptions">
+      </el-date-picker>
+    </div>
   </div>
 </template>
 
@@ -46,13 +30,41 @@ export default {
   name: "App",
   components: {},
   data() {
-    return {}
+    return {
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        },
+        shortcuts: [{
+          text: '今天',
+          onClick(picker) {
+            picker.$emit('pick', new Date());
+          }
+        }, {
+          text: '昨天',
+          onClick(picker) {
+            const date = new Date();
+            date.setTime(date.getTime() - 3600 * 1000 * 24);
+            picker.$emit('pick', date);
+          }
+        }, {
+          text: '一周前',
+          onClick(picker) {
+            const date = new Date();
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', date);
+          }
+        }]
+      },
+      value1: '',
+      value2: '',
+    };
   },
-  methods: {},
-  mounted() {
-    // console.log('app', this)
+  methods: {
+    showInfo() {
+      console.log(this.value1)
+    }
   }
-
 };
 </script>
 
