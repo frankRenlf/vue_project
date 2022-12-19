1937975808976376a39696beeb2e3b6109253209
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-19 14:09:04 +0800

    add

---
+++ vue_test_demo/43_src_hash_history/components/BannerComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header">
+      <h2>Vue Router Demo</h2>
+      <button @click="back">back</button>
+      <button @click="forward">forward</button>
+      <button @click="identify">identify</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BannerComponent",
+  methods: {
+    back() {
+      this.$router.back()
+    },
+    forward() {
+      this.$router.forward()
+    },
+    identify(){
+      localStorage.setItem("name","james")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_test_demo/43_src_hash_history/store/index.js
@@ -0,0 +1,14 @@
+import vuex from "vuex";
+import Vue from 'vue'
+import countOptions from './modules/count'
+import personOptions from './modules/person'
+
+Vue.use(vuex)
+
+export default new vuex.Store({
+    modules: {
+        countOptions,
+        personOptions
+    }
+})
+
