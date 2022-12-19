53f6a424fd5d1d2c25987a4ca154be97e82664f8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-19 14:13:14 +0800

    Update App.vue

--- vue_test_demo/src/App.vue
+++ vue_test_demo/src/App.vue
@@ -1,48 +1,18 @@
 <template>
   <div>
-    <div class="row">
-      <BannerComponent></BannerComponent>
-    </div>
-    <div class="row">
-      <div class="col-xs-2 col-xs-offset-2">
-        <div class="list-group">
-          <!--          原始使用a标签跳转多个页面,多页面应用-->
-          <!--          <a class="list-group-item active" href="./about.html">About</a>-->
-          <!--          <a class="list-group-item" href="./home.html">Home</a>-->
-          <router-link replace class="list-group-item" active-class="active" to="/home">Home</router-link>
-          <router-link replace class="list-group-item"
-                       active-class="active"
-                       :to="{name:'about'}"
-          >
-            About
-          </router-link>
 
-        </div>
-      </div>
-      <div class="col-xs-6">
-        <div class="panel">
-          <div class="panel-body">
-            <h3>router - component</h3>
-            <!--router-view确定视图的位置-->
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 
 <script>
 
-import BannerComponent from "@/components/BannerComponent.vue";
 
 
 export default {
   name: "App",
   components: {
 
-    BannerComponent
   },
   data() {
     return {}
