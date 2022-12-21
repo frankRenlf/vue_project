b4e7c8d2cc0c7bafd5aaef6cf68eb419058fa3bc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-21 10:48:00 +0800

    Update App.vue

--- vue3_basic/src/App.vue
+++ vue3_basic/src/App.vue
@@ -3,6 +3,9 @@
   <h4>{{ name }}</h4>
   <h4>{{ age }}</h4>
   <button @click="add">add</button>
+  <h3>{{ job.type }}</h3>
+  <h3>{{ job.salary }}</h3>
+  <button @click="modify">modify</button>
 </template>
 
 <script>
@@ -15,6 +18,15 @@ export default {
   setup() {
     let name = ref('frank')
     let age = ref(22)
+    let job = ref({
+      type: 'backend',
+      salary: 30
+    })
+
+    function modify() {
+      job.value.type = 'frontend'
+      job.value.salary = 50
+    }
 
     function add() {
       age.value++
@@ -25,7 +37,9 @@ export default {
     return {
       name,
       age,
-      add
+      add,
+      job,
+      modify
     }
 
     // return () => h('h1', 'null')
