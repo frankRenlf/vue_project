7ac1f3840a2901ebb4781e7b6efaa353e1277386
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-21 10:25:09 +0800

    add

---
+++ vue3_basic/02_src_setup/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h3>app component</h3>
+  <h4>{{ name }}</h4>
+  <h4>{{ age }}</h4>
+  <button @click="add">add</button>
+</template>
+
+<script>
+// import {h} from 'vue'
+
+export default {
+  name: 'App',
+  components: {},
+  setup() {
+    let name = 'frank'
+    let age = 22
+
+    function add() {
+      age = age + 1
+      alert("add")
+    }
+
+    return {
+      name,
+      age,
+      add
+    }
+
+    // return () => h('h1', 'null')
+  }
+}
+</script>
+
+<style>
+</style>
