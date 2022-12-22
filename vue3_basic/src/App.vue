2cde37fc8a93d9c2f33275a447e278fe0adcc132
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-22 21:55:36 +0800

    add

--- vue3_basic/src/App.vue
+++ vue3_basic/src/App.vue
@@ -2,22 +2,17 @@
   <h3>app component</h3>
   <h4>{{ person.name }}</h4>
   <h4>{{ person.age }}</h4>
-  <button @click="add">add</button>
-  <h3>{{ person.job.type }}</h3>
-  <h3>{{ person.job.salary }}</h3>
-  <h3 v-show="person.address">{{ person.address }}</h3>
-  <h3>{{ arr }}</h3>
-  <button @click="modify">modify</button>
-  <button @click="addAttribute">addAttribute</button>
+  <DemoComponent></DemoComponent>
 </template>
 
 <script>
 // import {h} from 'vue'
 import {ref, reactive} from "vue";
+import DemoComponent from "@/components/DemoComponent.vue";
 
 export default {
   name: 'App',
-  components: {},
+  components: {DemoComponent},
   setup() {
     let person = reactive({
       name: 'frank',
@@ -27,32 +22,9 @@ export default {
         salary: 30
       }
     })
-    let arr = reactive(['he', 'ha'])
-
-    function modify() {
-      console.log(person.job)
-      person.job.type = 'frontend'
-      person.job.salary = 50
-      arr[1] = 'hei'
-    }
-
-    function add() {
-      person.age++
-      console.log(name, person.age)
-      // alert("add")
-    }
-
-    function addAttribute() {
-      person.address = 'hz'
-      // alert("add")
-    }
 
     return {
-      arr,
-      add,
       person,
-      modify,
-      addAttribute
     }
 
     // return () => h('h1', 'null')
---
+++ vue3_basic/src/components/DemoComponent.vue
@@ -0,0 +1,13 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: "DemoComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
