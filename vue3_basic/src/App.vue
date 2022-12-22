f4918bdfa1e8d98af46fc6a02845b9ee2984828e
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-22 22:04:46 +0800

    add

--- vue3_basic/src/App.vue
+++ vue3_basic/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <h3>app component</h3>
-  <h4>{{ person.name }}</h4>
-  <h4>{{ person.age }}</h4>
-  <DemoComponent></DemoComponent>
+
+  <DemoComponent msg="farnk"></DemoComponent>
 </template>
 
 <script>
@@ -13,22 +12,7 @@ import DemoComponent from "@/components/DemoComponent.vue";
 export default {
   name: 'App',
   components: {DemoComponent},
-  setup() {
-    let person = reactive({
-      name: 'frank',
-      age: 22,
-      job: {
-        type: 'backend',
-        salary: 30
-      }
-    })
 
-    return {
-      person,
-    }
-
-    // return () => h('h1', 'null')
-  }
 }
 </script>
 
--- vue3_basic/src/components/DemoComponent.vue
+++ vue3_basic/src/components/DemoComponent.vue
@@ -1,10 +1,32 @@
 <template>
-<div></div>
+  <h4>{{ person.name }}</h4>
+  <h4>{{ person.age }}</h4>
+  <h5></h5>
 </template>
 
 <script>
+import {reactive} from "vue";
+
 export default {
-  name: "DemoComponent"
+  name: "DemoComponent",
+  props:['msg'],
+  setup(props) {
+    console.log(props)
+    let person = reactive({
+      name: 'frank',
+      age: 22,
+      job: {
+        type: 'backend',
+        salary: 30
+      }
+    })
+
+    return {
+      person,
+    }
+
+    // return () => h('h1', 'null')
+  }
 }
 </script>
 
