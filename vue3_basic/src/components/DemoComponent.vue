90ebe77145a12a246276fbe569566dde7cd4cf0b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-23 14:44:20 +0800

    add

--- vue3_basic/src/components/DemoComponent.vue
+++ vue3_basic/src/components/DemoComponent.vue
@@ -43,7 +43,7 @@ export default {
     //   immediate: true,
     //   deep: true,
     // })
-    watch(() => person.job.salary, (newVal, oldVal) => {
+    watch(() => person.job, (newVal, oldVal) => {
       console.log("person is change", newVal, oldVal)
     }, {
       immediate: true,
