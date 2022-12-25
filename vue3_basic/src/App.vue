f269d99321110bedc762a376901affaa02d54cad
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-25 11:48:12 +0800

    add

--- vue3_basic/src/App.vue
+++ vue3_basic/src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <h3>app component</h3>
-
-  <DemoComponent>
-    <template v-slot:d1>
-      <input type="text" placeholder="enter">
-    </template>
+  <button @click="show=!show">switch</button>
+  <DemoComponent v-if="show">
   </DemoComponent>
+
 </template>
 
 <script>
 // import {h} from 'vue'
-import {ref, reactive} from "vue";
+import {ref} from "vue";
 import DemoComponent from "@/components/DemoComponent.vue";
 
 export default {
   name: 'App',
   components: {DemoComponent},
-  setup(){
-    function show(val){
-      console.log(val)
-    }
+  setup() {
+    let show = ref(true)
     return {
       show
     }
--- vue3_basic/src/components/DemoComponent.vue
+++ vue3_basic/src/components/DemoComponent.vue
@@ -1,65 +1,40 @@
 <template>
   <h3>{{ sum }}</h3>
-  <button @click="sum++">show</button>
-  <hr>
-  <h3>{{ msg }}</h3>
-  <button @click="msg+='!'">add</button>
-  <hr>
-  <h3>{{ person.name }}</h3>
-  <h3>{{ person.age }}</h3>
-  <button @click="person.age++"> change</button>
-  <hr>
-  <h3>{{ person.job.salary }}</h3>
-  <button @click="person.job.salary++"> change</button>
+  <button @click="sum++">add</button>
+
 </template>
 
 <script>
-import {reactive, computed, ref, watch, watchEffect} from "vue";
+import {ref} from "vue";
 
 export default {
   name: "DemoComponent",
   setup(props, context) {
     let sum = ref(0)
-    let msg = ref('hello')
-    let person = reactive({
-      name: 'frank',
-      age: 22,
-      job: {
-        name: 'programmer',
-        salary: 30,
-      }
-    })
-
-    // watch(sum, (newVal, oldVal) => {
-    //       console.log("sum is change", newVal, oldVal)
-    //     }, {
-    //       immediate: true,
-    //       deep: true,
-    //     }
-    // )
-    // watch(person, (newVal, oldVal) => {
-    //   console.log("person is change", newVal, oldVal)
-    // }, {
-    //   immediate: true,
-    //   deep: true,
-    // })
-    // watch(person, (newVal, oldVal) => {
-    //   console.log("person is change", newVal, oldVal)
-    // }, {
-    //   immediate: true,
-    //   deep: true,
-    // })
-    watchEffect(() => {
-      let x = person.job.salary
-      console.log(x)
-    })
     return {
-      person,
       sum,
-      msg
     }
-
-    // return () => h('h1', 'null')
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
   }
 }
 </script>
