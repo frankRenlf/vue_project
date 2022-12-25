3987f796aaea90d7157812b514e7731234f931ad
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-25 17:16:06 +0800

    add

--- vue3_basic/src/components/DemoComponent.vue
+++ vue3_basic/src/components/DemoComponent.vue
@@ -12,39 +12,9 @@ export default {
   name: "DemoComponent",
   setup(props, context) {
     console.log('setup')
-    onBeforeMount(() => {
-      console.log('onBeforeMount')
-    })
 
-    function locate(event) {
-      point.x = event.pageX
-      point.y = event.pageY
-    }
-
-    onMounted(() => {
-      console.log('onMounted')
-      window.addEventListener('click', locate)
-    })
-    onBeforeUpdate(() => {
-      console.log('onBeforeUpdate')
-    })
-    onUpdated(() => {
-      console.log('onUpdated')
-    })
-    onBeforeUnmount(() => {
-      console.log('onBeforeUnmount')
-      window.removeEventListener('click', locate)
-    })
-    onUnmounted(() => {
-      console.log('onUnmounted')
-    })
-    let point = reactive({
-      x: 0,
-      y: 0
-    })
     let sum = ref(0)
     return {
-      point,
       sum,
     }
   },
