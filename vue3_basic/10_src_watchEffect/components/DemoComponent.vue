7e9c1eace06a5b46c6f4369863e6cca31ec2e425
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-25 12:03:09 +0800

    add

---
+++ vue3_basic/10_src_watchEffect/components/DemoComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <h3>{{ sum }}</h3>
+  <button @click="sum++">add</button>
+
+</template>
+
+<script>
+import {onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref} from "vue";
+
+export default {
+  name: "DemoComponent",
+  setup(props, context) {
+    console.log('setup')
+    onBeforeMount(() => {
+      console.log('onBeforeMount')
+    })
+    onMounted(() => {
+      console.log('onMounted')
+    })
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate')
+    })
+    onUpdated(() => {
+      console.log('onUpdated')
+    })
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount')
+    })
+    onUnmounted(() => {
+      console.log('onUnmounted')
+    })
+    let sum = ref(0)
+    return {
+      sum,
+    }
+  },
+  /*
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+  }
+   */
+}
+</script>
+
+<style scoped>
+
+</style>
