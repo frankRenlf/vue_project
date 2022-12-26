c5071e6b2599c5dbf00e60cdc8ef5f46ea585d41
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-26 18:35:59 +0800

    add

---
+++ vue3_basic/16_src_toRaw_markRaw/components/DemoComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <h3>{{ sum }}</h3>
+  <button @click="sum++"> change</button>
+  <hr>
+  <h3>{{ person.name }}</h3>
+  <button @click="person.name+='?'"> change</button>
+  <hr>
+  <h3>{{ person.age }}</h3>
+  <button @click="person.age++"> change</button>
+  <hr>
+  <h3>{{ person.job.salary }}</h3>
+  <button @click="person.job.salary++"> change</button>
+  <hr>
+  <h3>{{ person.car }}</h3>
+  <button @click="addCar"> add car</button>
+  <button @click="modifyCar"> modify car</button>
+</template>
+
+<script>
+import {reactive, ref, readonly, markRaw} from "vue";
+
+export default {
+  name: "DemoComponent",
+  setup(props, context) {
+    let sum = ref(0)
+    let person = reactive({
+      name: 'frank',
+      age: 22,
+      job: {
+        name: 'programmer',
+        salary: 30,
+      }
+    })
+
+    function addCar() {
+      person.car = {
+        name: 'bwm',
+        price: 40,
+      }
+      console.log(person.car)
+    }
+
+    function modifyCar() {
+      person.car.name = 'bc'
+      person.car.price = 50
+    }
+
+    return {
+      person,
+      sum,
+      addCar, modifyCar
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
