e7cda02b625c8e34845a791810c9b9fe2f387dc8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-26 15:12:39 +0800

    add

---
+++ vue3_basic/14_src_shallowReactive_shallowRef/components/DemoComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>{{ person.name }}</h3>
+  <button @click="person.name+='?'"> change</button>
+  <hr>
+  <h3>{{ person.age }}</h3>
+  <button @click="person.age++"> change</button>
+  <hr>
+  <h3>{{ person.job.salary }}</h3>
+  <button @click="person.job.salary++"> change</button>
+</template>
+
+<script>
+import {reactive, watch, toRefs, shallowRef, shallowReactive} from "vue";
+
+export default {
+  name: "DemoComponent",
+  setup(props, context) {
+
+    let person = shallowReactive({
+      name: 'frank',
+      age: 22,
+      job: {
+        name: 'programmer',
+        salary: 30,
+      }
+    })
+
+    return {
+      person,
+    }
+
+    // return () => h('h1', 'null')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
