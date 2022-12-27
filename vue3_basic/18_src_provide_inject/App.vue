4fcc1ed238d0717353aca0c64fd678adea05ee42
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-27 18:55:58 +0800

    add

---
+++ vue3_basic/18_src_provide_inject/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app">
+    <h3>app component</h3>
+    <button @click="show=!show">switch</button>
+    <hr>
+    <DemoComponent v-if="show">
+    </DemoComponent>
+  </div>
+
+
+</template>
+
+<script>
+// import {h} from 'vue'
+import {ref, provide, reactive} from "vue";
+import DemoComponent from "@/components/DemoComponent.vue";
+
+export default {
+  name: 'App',
+  components: {DemoComponent},
+  setup() {
+    let show = ref(true)
+    let car = reactive({
+      name: 'bc',
+      price: 40
+    })
+    provide('car',car)
+    return {
+      show
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.app {
+  background-color: green;
+  padding: 10px;
+}
+</style>
---
+++ vue3_basic/18_src_provide_inject/components/ChildComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="child">
+    <h3>child {{car}}</h3>
+  </div>
+
+</template>
+
+<script>
+import {inject} from "vue";
+
+export default {
+  name: "ChildComponent",
+  setup() {
+    const car = inject('car')
+    return {car}
+  }
+}
+</script>
+
+<style scoped>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
