6ead10d3f15144ec8a697eb153b0c5993fe49c49
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-27 14:30:27 +0800

    add

---
+++ vue3_basic/17_src_customRef/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>app component</h3>
+  <button @click="show=!show">switch</button>
+  <hr>
+  <DemoComponent v-if="show">
+  </DemoComponent>
+
+</template>
+
+<script>
+// import {h} from 'vue'
+import {ref} from "vue";
+import DemoComponent from "@/components/DemoComponent.vue";
+
+export default {
+  name: 'App',
+  components: {DemoComponent},
+  setup() {
+    let show = ref(true)
+    return {
+      show
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
