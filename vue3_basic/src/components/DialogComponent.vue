af340c99667f7d83253813cc6c14ba9628436b26
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-27 22:34:36 +0800

    Update DialogComponent.vue

--- vue3_basic/src/components/DialogComponent.vue
+++ vue3_basic/src/components/DialogComponent.vue
@@ -1,9 +1,11 @@
 <template>
   <button @click="isShow=true">show</button>
   <teleport to="body">
-    <div v-if="isShow" class="dialog">
-      <input type="text" placeholder="enter">
-      <button @click="isShow=false">close</button>
+    <div v-if="isShow" class="mask">
+      <div class="dialog">
+        <input type="text" placeholder="enter">
+        <button @click="isShow=false">close</button>
+      </div>
     </div>
   </teleport>
 </template>
@@ -22,7 +24,21 @@ export default {
 </script>
 
 <style scoped>
+.mask {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
 .dialog {
+  position: absolute;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
   width: 300px;
   height: 50px;
   background-color: pink;
