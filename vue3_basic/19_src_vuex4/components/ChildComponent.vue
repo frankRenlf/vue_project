1e2d23e8c3297f15ba2b5a390f82fedcce6589c1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-27 22:24:46 +0800

    add

--- vue3_basic/19_src_vuex4/components/ChildComponent.vue
+++ vue3_basic/19_src_vuex4/components/ChildComponent.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="child">
     <h3>child {{car}}</h3>
-    <DialogComponent></DialogComponent>
   </div>
 
 </template>
 
 <script>
 import {inject} from "vue";
-import DialogComponent from "@/components/DialogComponent.vue";
+
 export default {
   name: "ChildComponent",
-  components:{
-    DialogComponent
-  },
   setup() {
     const car = inject('car')
     return {car}
---
+++ vue3_basic/src/components/DialogComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="isShow=true">show</button>
+  <teleport to="body">
+    <div v-if="isShow" class="dialog">
+      <input type="text" placeholder="enter">
+      <button @click="isShow=false">close</button>
+    </div>
+  </teleport>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "DialogComponent",
+  setup() {
+    return {
+      isShow: ref(false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dialog {
+  width: 300px;
+  height: 50px;
+  background-color: pink;
+  padding: 10px;
+}
+</style>
